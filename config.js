9dc9508202cbe30bc9498559f72acaacd5adca07
Namgune16 <87465138+Namgune16@users.noreply.github.com>
2021-07-15 13:49:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'Cun be CTrần cute phô mai wee ! ',
+    introDesc: `Bố mài biết là mài đang ỉ nên hãy nín lại xíu bố có cái nay cho mai xem nek :> `,
+    btnIntro: ' NíN lun ',
+    title: 'Lawu wa rui khum gặp 😙',
+    desc: ' Khum pik con be de thuong xinh gai su bíu ti fun này còn súm khum ? ',
+    btnYes: 'Còn, Đang ia metch gke :b',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại ỉa vậy =)) ',
+    btnReply: 'Gửi cho Hoai Nam dep trai hihi <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Toi pik ban dang ia mal hoi b@n ia ik 😘😘',
+    messDesc: 'Chuk b@n ia zui vek nhak :333 .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' 
 }
